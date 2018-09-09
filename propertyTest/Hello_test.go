638bff0adc2664e6d852481254af18e9b4dc9b66
jacky1234 <jackdoson2011@gmail.com>
2018-09-09 20:26:04 +0800

    testing

--- propertyTest/Hello_test.go
+++ propertyTest/Hello_test.go
@@ -1,8 +1,13 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"os"
+)
 
 /**
+单元测试示例
 性能测试示例
  */
 func add(x, y int) int {
@@ -25,4 +30,24 @@ func BenchmarkMinus(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = minus(1, 2)
 	}
+}
+
+func TestAdd(t *testing.T) {
+	if add(1, 2) != 4 {
+		t.FailNow()
+	}
+}
+
+//Parallel 可有效利用多核并行优势，缩短测试时间
+func TestA(t *testing.T) {
+	t.Parallel()
+	time.Sleep(time.Second * 2)
+}
+
+func TestB(t *testing.T) {
+	if os.Args[len(os.Args) - 1)] == "b"{
+		t.Parallel()
+	}
+
+	time.Sleep(time.Second * 2)
 }
